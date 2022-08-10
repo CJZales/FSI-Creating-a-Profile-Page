cc60bb5e7d9961ff372a21ee696d5fdcfbb8785e
CJ <cjdegonzales@gmail.com>
2022-08-09 21:23:35 -0700

    done

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 let content = document.querySelector('.js-generated.content')
+
 let header = document.createElement('h1')          
 header.setAttribute('class','dog-name')
 header.append('Rizzo')
 content.append(header)
 
+let dogContent = document.createElement('dog-content')
+dogContent.setAttribute('class', 'dog-content')
+content.append(dogContent)
 
+let dogImage = document.createElement('dog-image')
+dogImage.setAttribute('class','dog-image')
+content.append(dog-image)
+
+let dogDetails = document.createElement('dog-details')
+dogDetails.setAttribute('class','dog-details')
+content.append(dog-details)
