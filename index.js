7f46dd12f3acd9a20977407e8341dc8a1bcea02b
CJ <cjdegonzales@gmail.com>
2022-08-09 21:06:55 -0700

    done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let content = document.querySelector('.js-generated.content')
-let h1 class = document.createElement('h1')          
-h1.setAttribute('class','dog-name')
-h1.append('Rizzo')
+let header = document.createElement('h1')          
+header.setAttribute('class','dog-name')
+header.append('Rizzo')
